cd28911b4b3655f0a2e893dda7343d8bfa0073ee
Philippe Aucazou <paucazou@yahoo.fr>
2020-05-03 18:48:00 +0200

    Added some options

--- build.sh
+++ build.sh
@@ -5,5 +5,27 @@ build_mock () {
     if [[ ! $compiler ]]; then
         compiler=g++
     fi
-    $compiler -pedantic -Wall -Wextra -std=c++17 -I ./mock encoder.cpp main.cpp ./mock/wiringPi.cpp ./mock/RtMidi.cpp -o encoder -D DEBUG -D LOOP_NB=false
+    $compiler -pedantic -Wall -Wextra -std=c++17 -I ./mock encoder.cpp main.cpp ./mock/wiringPi.cpp ./mock/RtMidi.cpp -o encoder -D DEBUG -D LOOP_NB=false -D MOCK
 }
+
+build_debug () {
+    g++ -pedantic -Wall -Wextra -std=c++17\
+        -D DEBUG\
+        -I /usr/include/rtmidi/ \
+        -I /usr/include/\
+        -lasound -lpthread -lrtmidi -lwiringpi\
+        -D__LINUX_ALSA__\
+        encoder.cpp main.cpp\
+        -o encoder
+}
+        
+
+build_release () {
+    g++ -std=c++17\
+        -I /usr/include/rtmidi/ \
+        -I /usr/include/\
+        -lasound -lpthread -lrtmidi -lwiringpi\
+        -D__LINUX_ALSA__\
+        encoder.cpp main.cpp\
+        -o encoder
+    }
